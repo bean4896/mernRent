df93ef1d2a03d8bb01241d00ef3b9578d7c4903f
GasG <dkybone1996@gmail.com>
2022-11-07 11:39:27 +0800

    add logOut

--- components/layout/Navigation.jsx
+++ components/layout/Navigation.jsx
@@ -1,32 +1,81 @@
-import Toggle from './toggle';
-import Link from 'next/link';
-import { useSession } from 'next-auth/react';
-const Navbar = () => {
-  const { data: session, status } = useSession()
+import Toggle from "./toggle";
+import { useEffect } from "react";
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-    return (
-      <header className="bg-white dark:bg-zinc-900 border-b-[0.5px] dark:border-neutral-600 ">
-        <div className='nav-container'>
+const Navbar = () => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  function logOutHandler(e) {
+    e.preventDefault();
+    signOut(); 
+  }
+
+  // const logOutHandler = useEffect(() => {
+  //   if (status === "authenticated") {
+  //     router.push("/");
+  //     signOut();
+  //   }
+  // }, [status]);
+
+
+  return (
+    <header className="bg-white dark:bg-zinc-900 border-b-[0.5px] dark:border-neutral-600 ">
+      <div className="nav-container">
         <nav className="flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row justify-between">
-         <div>
-            <Link href='/'><h1> Home Page</h1></Link>
-         </div>
-  
-          <div className='flex flex-row align-middle'> 
-            <ul className='flex flex-row space-x-4'>
-              { status === 'authenticated' &&  <div>{session.user.email}</div> }
-              { status === 'authenticated' && <Link href='/newmemory'><button className='btn rounded-md text-yellow-400 bg-yellow-50 dark:bg-yellow-900'>Add Item</button></Link>}
-             { !status === 'authenticated' &&<Link href='/auth'><button className='btn rounded-md text-yellow-400 bg-yellow-50 dark:bg-yellow-900'>Login To Add</button></Link>} 
-              <Link href='/auth'><button className='btn rounded-md text-[#ffffff] bg-purple-900 dark:text-yellow-400'>Register</button></Link>
+          <div>
+            <Link href="/">
+              <h1> Home Page</h1>
+            </Link>
+          </div>
+
+          <div className="flex flex-row align-middle">
+            <ul className="flex flex-row space-x-4">
+              {session && <div>{session.user.email}</div>}
+
+              {session && (
+                <Link href="/newmemory">
+                  <button className="btn rounded-md text-yellow-400 bg-yellow-50 dark:bg-yellow-900">
+                    Add Item
+                  </button>
+                </Link>
+              )}
+
+              {!session && (
+                <Link href="/auth">
+                  <button className="btn rounded-md text-yellow-400 bg-yellow-50 dark:bg-yellow-900">
+                    Login
+                  </button>
+                </Link>
+              )}
+
+              {!session && (
+                <Link href="/auth">
+                  <button className="btn rounded-md text-[#ffffff] bg-purple-900 dark:text-yellow-400">
+                    Register
+                  </button>
+                </Link>
+              )}
+              
+              {session && (
+                <Link href="/auth">
+                  <button
+                    onClick={logOutHandler}
+                    className="btn rounded-md text-[#ffffff] bg-purple-900 dark:text-yellow-400"
+                  >
+                    Logout
+                  </button>
+                </Link>
+              )}
             </ul>
             <Toggle />
           </div>
         </nav>
-        </div>
-      </header>
-    );
-  }
-
-
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
--- components/memories/MemoriesList.jsx
+++ components/memories/MemoriesList.jsx
@@ -1,10 +1,8 @@
 import MemoryItem from "./MemoryItem";
 
-
-
 const MemoriesList = (props) => {    
     return (
-        <div className=" masonry-2-col ">
+        <div className="columns-2 ">
             {props.memories.map((memory) => (
                 // MemoryItem is a component that takes in a memory object as a prop
                 <MemoryItem
